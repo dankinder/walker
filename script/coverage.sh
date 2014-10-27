338c8066e427a85dbeb65132cb4e32f1b388dc49
dkinder <dkinder.is.me@gmail.com>
2014-10-27 15:29:27 -0700

    Added .travis.yml
    This may not work the first time, I'll probably push directly if it
    isn't happy for some reason. Need to get it in there to see though.

--- script/coverage.sh
+++ script/coverage.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 ## This code will write coverage html files. The files will end up as test/coverage.{out,html} and console/test/coverage.{out,html}
 go test github.com/iParadigms/walker/console/test -cover -coverpkg github.com/iParadigms/walker/console -coverprofile=console/test/coverage.out && \
 go tool cover -html=console/test/coverage.out -o console/test/coverage.html && \
--- script/test.sh
+++ script/test.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env sh
 sudo -E go test -tags "sudo cassandra" github.com/iParadigms/walker/test -cover -coverpkg github.com/iParadigms/walker
