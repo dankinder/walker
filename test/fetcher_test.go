fbfb81692e21f7cba193cd60d54b39e8a8394f13
pchristopher <pchristopher@iparadigms.com>
2014-12-10 10:10:59 -0800

    Added a proto test for the case when domain is host:port in DispatchLinkFilter.

--- test/fetcher_test.go
+++ test/fetcher_test.go
@@ -329,6 +329,11 @@ func TestUrlParsing(t *testing.T) {
 			input:  "http://a.com/page1.com?PHPSESSID=436100313FAFBBB9B4DC8BA3C2EC267B",
 			expect: "http://a.com/page1.com",
 		},
+		{
+			tag:    "EmbeddedPort",
+			input:  "http://a.com:8080/page1.com",
+			expect: "http://a.com:8080/page1.com",
+		},
 	}
 
 	for _, tst := range tests {
