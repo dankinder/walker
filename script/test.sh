3eee8d34857d84a45d9a15d9b3e4add76f3642ea
dkinder <dkinder.is.me@gmail.com>
2015-01-07 14:42:53 -0800

    Updated script/test.sh to work from anywhere

--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+# Change directory to the root of this walker repository
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
 # -p 1 ensures that multiple test binaries from different subpackages don't run
 # in parallel. We need this because multiple packages test with the local
 # cassandra instance and can conflict.
-sudo -E $(which go) test -p 1 -tags "sudo cassandra" -cover github.com/iParadigms/walker/...
+sudo -E $(which go) test -p 1 -tags "sudo cassandra" -cover ./...
